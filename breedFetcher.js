a2f2bba66f7700dcd2271dd2705f2e1c68ddd5bf
bhalemegha <meghabhale6@gmail.com>
2021-11-04 23:06:22 +0000

    breedfetcher with index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,26 +1,23 @@
 const request = require("request");
-const breedFetcher = function (breedName, callback) {
+const fetchBreedDescription = function (breedName, callback) {
   let breedId = breedName.substring(0, 4).toLowerCase();
   request('https://api.thecatapi.com/v1/images/search?breed_ids=' + breedId, (error, response, body) => {
     const data = (JSON.parse(body))[0];
     if (error) {
-      callback(error);
+      callback(error,body);
       return;
-    }console.log(body)
+    }
     if (body.length > 2) {
       for (let d in data) {
         if (data[d][0]['id'] === breedId) {
-          callback(data[d][0]['description']);
+          callback(error,data[d][0]['description']);
           break;
         }
       }
     } else {
-      callback("Breed not found");
+      callback(error,"Breed not found");
     }
   });
 }
 
-breedFetcher('Siberian', (data) => {
-                                    console.log(data)
-                                  });
-
+module.exports = { fetchBreedDescription };
