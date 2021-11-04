f9bc978be5b9d6e61ef95ac87d0386cf602a6f85
bhalemegha <meghabhale6@gmail.com>
2021-11-04 22:13:37 +0000

    Breed Fetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,24 @@
 const request = require("request");
-const breedFetcher = function (breedName) {
+const breedFetcher = function (breedName, callback) {
   let breedId = breedName[0].substring(0, 4);
   request('https://api.thecatapi.com/v1/images/search?breed_ids=' + breedId, (error, response, body) => {
     const data = (JSON.parse(body))[0];
     if (error) {
-      console.log(error);
+      callback(error);
       return;
     }
     if (body.length > 2) {
       for (let d in data) {
         if (data[d][0]['id'] === breedId) {
-          console.log(data[d][0]['description']);
+          callback(data[d][0]['description']);
           break;
         }
       }
     } else {
-      console.log("Breed not found");
+      callback("Breed not found");
     }
   });
 }
 
-breedFetcher(process.argv.splice(2));
+breedFetcher(process.argv.splice(2), (data) => {console.log(data)});
 
