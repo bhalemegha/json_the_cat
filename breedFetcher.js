a16bef890e742425fbeea40cdcd0aa83c6ac6b86
bhalemegha <meghabhale6@gmail.com>
2021-11-04 22:25:56 +0000

    Breed Fetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,12 @@
 const request = require("request");
 const breedFetcher = function (breedName, callback) {
-  let breedId = breedName[0].substring(0, 4);
+  let breedId = breedName.substring(0, 4).toLowerCase();
   request('https://api.thecatapi.com/v1/images/search?breed_ids=' + breedId, (error, response, body) => {
     const data = (JSON.parse(body))[0];
     if (error) {
       callback(error);
       return;
-    }
+    }console.log(body)
     if (body.length > 2) {
       for (let d in data) {
         if (data[d][0]['id'] === breedId) {
@@ -20,5 +20,7 @@ const breedFetcher = function (breedName, callback) {
   });
 }
 
-breedFetcher(process.argv.splice(2), (data) => {console.log(data)});
+breedFetcher('Siberian', (data) => {
+                                    console.log(data)
+                                  });
 
